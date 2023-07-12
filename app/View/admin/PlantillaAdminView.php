1054c65b3be17da2acca504ac4a6f66b4534e0e0
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-07-12 12:12:29 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   app/View/admin/PlantillaAdminView.php
    
     Changes not staged for commit:
    modified:   app/Controller/DefaultController.php
    	modified:   app/Controller/UserController.php
    	modified:   app/Model/UserModel.php
    	deleted:    app/View/HomeLoginView.php
    	modified:   app/View/admin/Users/EditUserView.php
    	deleted:    app/src/img/avatars/Avatar_64a8576fd3d6c.jpeg
    
     Untracked files:
    	app/View/admin/HomeLoginView.php
    	app/View/admin/Plantilla2AdminView.php

--- app/View/admin/PlantillaAdminView.php
+++ app/View/admin/PlantillaAdminView.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Catando Ando Coffee Shop</title>
-    <link rel="stylesheet" href="app/View/style.css" />
-  </head>
-  <body>
-    <header class="header">
-      <h1>Catando Ando Coffee Shop</h1>
-      <nav class="navbar">
-        <ul>
-          <li><a href="http://localhost/php3b">Inincio</a></li>
-          <li><a href="http://localhost/php3b/?C=ProductsController&M=index">Productos</a></li>
-          <li><a href="http://localhost/php3b/?C=VarietalsController&M=index">Variedades</a></li>
-          <li><a href="http://localhost/php3b/?C=UserController&M=index">Usiarios</a></li>
-          <li><a href="http://localhost/php3b/?C=PresentsController&M=index">Presentacion</a></li>
-          <li><a href="http://localhost/php3b/?C=UserController&M=CallFormLogin">Login</a></li>
-        </ul>
-      </nav>
-    </header>
-    <section class="content">
-      <!--en esta parte es lo que va a cambiar de nuestra plantilla -->
-      <?php include_once($vista) ?>
-    </section>
-    <footer class="footer">
-      <h3>derechos reservados</h3>
-    </footer>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Catando Ando Coffee Shop</title>
+  <link rel="stylesheet" href="app/View/style.css" />
+</head>
+
+<body>
+  <header class="header">
+    <h1>Catando Ando Coffee Shop</h1>
+    <nav class="navbar">
+      <ul>
+        <li><a href="http://localhost/php3b">Inincio</a></li>
+        <li><a href="http://localhost/php3b/?C=ProductsController&M=index">Productos</a></li>
+        <li><a href="http://localhost/php3b/?C=VarietalsController&M=index">Variedades</a></li>
+        <li><a href="http://localhost/php3b/?C=UserController&M=index">Usiarios</a></li>
+        <li><a href="http://localhost/php3b/?C=PresentsController&M=index">Presentacion</a></li>
+        <li><a href="http://localhost/php3b/?C=UserController&M=CallFormLogin">Login</a></li>
+      </ul>
+    </nav>
+  </header>
+  <section class="content">
+    <!--en esta parte es lo que va a cambiar de nuestra plantilla -->
+    <?php include_once($vista) ?>
+  </section>
+  <footer class="footer">
+    <h3>derechos reservados</h3>
+  </footer>
+</body>
+
 </html>
